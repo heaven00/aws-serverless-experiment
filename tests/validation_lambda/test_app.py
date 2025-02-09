cb98a8ec6411cb047856a2f86420761e01af558f
jayant <jayant746@gmail.com>
2025-02-09 14:28:54 -0500

    ci/cd pipeline to build and deploy ecr images

--- tests/validation_lambda/test_app.py
+++ tests/validation_lambda/test_app.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from src.validation_lambda.app import valid_transaction_handler, Status
+from src.validation_lambda.app import valid_transaction, Status
 
 # Helper function to encode payload
 
@@ -13,7 +13,7 @@ def decode_payload(payload):
     return payload
 
 # Sample valid transaction
-def valid_transaction():
+def dummy_valid_transaction():
     return {
         "transaction_id": "T12345",
         "user_id": "U56789",
@@ -28,35 +28,34 @@ def valid_transaction():
 
 
 # Test case for valid transaction
-def test_valid_transaction():
-    event = {"Records": [{"kinesis": {"data": encode_payload(valid_transaction())}}]}
+def test_valid_transaction_should_process():
+    event = {"Records": [{"kinesis": {"data": encode_payload(dummy_valid_transaction())}}]}
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
 
     assert len(response) == 1
-    print(response)
     assert response[0]["status"] == Status.ok.value
     assert response[0]["error"] == ""
 
 
 # Test case for invalid transaction with missing fields
-def test_invalid_transaction_missing_fields():
-    invalid_payload = valid_transaction()
+def test_invalid_transaction_missing_fields_should_return_empty_response():
+    invalid_payload = dummy_valid_transaction()
     del invalid_payload["transaction_id"]  # Remove a required field
     event = {"Records": [{"kinesis": {"data": encode_payload(invalid_payload)}}]}
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
 
     assert len(response) == 0
 
 
 # Test case for invalid transaction with incorrect format
-def test_invalid_transaction_incorrect_format():
-    invalid_payload = valid_transaction()
+def test_invalid_transaction_incorrect_format_should_return_empty_response():
+    invalid_payload = dummy_valid_transaction()
     invalid_payload["transaction_id"] = "12345"  # Does not match regex
     event = {"Records": [{"kinesis": {"data": encode_payload(invalid_payload)}}]}
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
 
     assert len(response) == 0
 
@@ -65,29 +64,29 @@ def test_invalid_transaction_incorrect_format():
 def test_multiple_transactions():
     event = {
         "Records": [
-            {"kinesis": {"data": encode_payload(valid_transaction())}},
-            {"kinesis": {"data": encode_payload(valid_transaction())}},
+            {"kinesis": {"data": encode_payload(dummy_valid_transaction())}},
+            {"kinesis": {"data": encode_payload(dummy_valid_transaction())}},
         ]
     }
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
 
     assert len(response) == 2
     assert all(tx["status"] == Status.ok.value for tx in response)
 
 
-def test_multiple_mix_transactions():
-    invalid_payload = valid_transaction()
+def test_multiple_mix_transactions_should_filter_invalid_transactions():
+    invalid_payload = dummy_valid_transaction()
     invalid_payload["transaction_id"] = "12345"
     event = {
         "Records": [
-            {"kinesis": {"data": encode_payload(valid_transaction())}},
-            {"kinesis": {"data": encode_payload(valid_transaction())}},
+            {"kinesis": {"data": encode_payload(dummy_valid_transaction())}},
+            {"kinesis": {"data": encode_payload(dummy_valid_transaction())}},
             {"kinesis": {"data": encode_payload(invalid_payload)}},
         ]
     }
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
 
     assert len(response) == 2
 
@@ -96,16 +95,16 @@ def test_output_is_json_serializable():
     """Lambda expects output to be serializable"""
     event = {
         "Records": [
-            {"kinesis": {"data": encode_payload(valid_transaction())}},
+            {"kinesis": {"data": encode_payload(dummy_valid_transaction())}},
         ]
     }
 
-    response = decode_payload(valid_transaction_handler(event, None))['data']
+    response = decode_payload(valid_transaction(event, None))['data']
     assert json.dumps(response)
 
 
 # Test case for empty event
 def test_empty_event():
     event = {"Records": []}
-    response = valid_transaction_handler(event, None)
+    response = valid_transaction(event, None)
     assert response == {'data': []}
